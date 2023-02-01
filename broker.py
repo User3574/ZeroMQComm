9fd37a6d2945a8fab63f0846a753887f78da1ddb
Jakub Beránek <berykubik@gmail.com>
2023-02-01 12:45:11 +0100

    Use asynchronous sending and receiving of requests

--- broker.py
+++ broker.py
@@ -12,21 +12,24 @@ if __name__ == '__main__':
     frontend.bind("tcp://*:5559")
     # Create two workers
     backend.bind("tcp://*:5560")
-    #backend.bind("tcp://*:5561")
 
-    # Initialize poll set
-    poller = zmq.Poller()
-    poller.register(frontend, zmq.POLLIN)
-    poller.register(backend, zmq.POLLIN)
-
-    # Switch messages between sockets
-    while True:
-        socks = dict(poller.poll())
-
-        if socks.get(frontend) == zmq.POLLIN:
-            message = frontend.recv_multipart()
-            backend.send_multipart(message)
-
-        if socks.get(backend) == zmq.POLLIN:
-            message = backend.recv_multipart()
-            frontend.send_multipart(message)
+    zmq.proxy(frontend, backend)
+    #
+    # # Initialize poll set
+    # poller = zmq.Poller()
+    # poller.register(frontend, zmq.POLLIN)
+    # poller.register(backend, zmq.POLLIN)
+    #
+    # # Switch messages between sockets
+    # while True:
+    #     socks = dict(poller.poll())
+    #
+    #     if socks.get(frontend) == zmq.POLLIN:
+    #         message = frontend.recv_multipart()
+    #         print("Got request from client, sending to worker")
+    #         backend.send_multipart(message)
+    #
+    #     if socks.get(backend) == zmq.POLLIN:
+    #         message = backend.recv_multipart()
+    #         print("Got response from worker, sending to client")
+    #         frontend.send_multipart(message)
--- client.py
+++ client.py
@@ -3,6 +3,7 @@
 #   Connects REQ socket to tcp://localhost:5559
 #   Sends "Hello" to server, expects "World" back
 #
+import tqdm as tqdm
 import zmq
 import numpy as np
 from utils import send_array
@@ -12,12 +13,23 @@ from numpy import random
 if __name__ == '__main__':
     #  Prepare our context and sockets
     context = zmq.Context()
-    socket = context.socket(zmq.REQ)
+    socket = context.socket(zmq.DEALER)
     socket.connect("tcp://localhost:5559")
 
-    #  Do 10 requests, waiting each time for a response
-    for request in range(1, 11):
+    # TODO: overlap sending of requests and receiving of messages to avoid a deadlock
+
+    # Send all requests
+    msg_count = 5
+    # msg_count = 500000 # this should work, but currently it deadlocks
+
+    for request in range(0, msg_count):
         data = np.array([random.randint(100) for x in range(10)])
-        send_array(socket, data)
-        message = socket.recv_string()
-        print(f"REP {request}: [{message}]")
+        # send_array(socket, data)
+        socket.send_string("A" * 10000)
+
+    # Wait for responses
+    for request in tqdm.tqdm(range(0, msg_count)):
+        worker, message = socket.recv_multipart()
+        # message = socket.recv_string()
+        # print(f"REP {request}: [{message}]")
+        # print(f"Got {message.decode()} computed by worker {worker.decode()}")

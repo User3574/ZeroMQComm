ad6d298f660ddf8ad8ba9c1a85c508d25a8493b2
User3574 <69751521+User3574@users.noreply.github.com>
2023-02-07 09:18:48 +0100

    ADD: Classes

--- client.py
+++ client.py
@@ -5,40 +5,63 @@ from utils import send_array
 from numpy import random
 
 
+class Client:
+    def __init__(self, addr="tcp://localhost:5559", port=8192):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.DEALER)
+
+        self.socket.set(zmq.SNDHWM, port)
+        self.socket.set(zmq.RCVHWM, port)
+        self.socket.connect(addr)
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN | zmq.POLLOUT)
+
+    def compute(self, array):
+        msg_send = 0
+        msg_received = 0
+        msg_count = len(array)
+        results = {}
+
+        # Switch messages between sockets
+        while msg_received < msg_count:
+            socks = dict(self.poller.poll())
+
+            if (socks.get(self.socket) & zmq.POLLIN) == zmq.POLLIN:
+                message_id, message = self.socket.recv_multipart()
+
+                # Decode
+                message_id = message_id.decode()
+                message = message.decode()
+
+                # Append
+                results[message_id] = message
+                msg_received += 1
+                if msg_received % 1000 == 0:
+                    print(f"R{msg_received}/{msg_count}")
+
+            if (socks.get(self.socket) & zmq.POLLOUT) == zmq.POLLOUT:
+                if msg_send % 1000 == 0:
+                    print(f"S{msg_send}/{msg_count}")
+
+                # Send until all messages are sent
+                self.socket.send_multipart([
+                    str(msg_send).encode(),
+                    array[msg_send].encode()
+                ])
+                msg_send += 1
+                if msg_send == msg_count:
+                    self.poller.modify(self.socket, zmq.POLLIN)
+
+        return results
+
+
 if __name__ == '__main__':
-    #  Prepare our context and sockets
-    context = zmq.Context()
-    socket = context.socket(zmq.DEALER)
-    socket.set(zmq.SNDHWM, 8192)
-    socket.set(zmq.RCVHWM, 8192)
-    socket.connect("tcp://localhost:5559")
+    client = Client()
 
-    # Send all requests
-    msg_count = 500000
-    data = "A" * 10000
+    # Create dummy array
+    msg_count = 100
+    array = ["A" * x for x in range(msg_count)]
 
-    msg_send = 0
-    msg_received = 0
-    poller = zmq.Poller()
-    poller.register(socket, zmq.POLLIN | zmq.POLLOUT)
-
-    # Switch messages between sockets
-    while msg_received < msg_count:
-        socks = dict(poller.poll())
-
-        if (socks.get(socket) & zmq.POLLIN) == zmq.POLLIN:
-            # print("Receiving")
-            worker, message = socket.recv_multipart()
-            # print(worker)
-            msg_received += 1
-            if msg_received % 1000 == 0:
-                print(f"R{msg_received}/{msg_count}")
-
-        if (socks.get(socket) & zmq.POLLOUT) == zmq.POLLOUT:
-            if msg_send % 1000 == 0:
-                print(f"S{msg_send}/{msg_count}")
-            socket.send_string(data)
-            # Send until all messages are sent
-            msg_send += 1
-            if msg_send == msg_count:
-                poller.modify(socket, zmq.POLLIN)
+    results = client.compute(array)
+    print(results)
--- worker.py
+++ worker.py
@@ -5,20 +5,40 @@ import json
 import numpy as np
 from utils import recv_array
 
+
+class Worker:
+    def __init__(self, func, addr="tcp://localhost:5560"):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.DEALER)
+        self.socket.connect(addr)
+        self.func = func
+
+    def run(self):
+        message_id = 0
+
+        while True:
+            id, message = self.socket.recv_multipart()
+
+            # Decode
+            id = id.decode()
+            message = message.decode()
+
+            # Compute
+            result = self.func(message)
+
+            # Send
+            self.socket.send_multipart([
+                str(id).encode(),
+                str(result).encode()
+            ])
+
+            message_id += 1
+            print(f'Sending {message_id}')
+
+
 if __name__ == '__main__':
-    context = zmq.Context()
-    socket = context.socket(zmq.DEALER)
-    socket.connect("tcp://localhost:5560")
+    def length(message):
+        return len(message)
 
-    worker_id = random.randint(1, 10)
-    print(f"Worker {worker_id}")
-    message_id = 0
-
-    while True:
-        message = socket.recv_multipart()[0]
-        socket.send_multipart([
-            str(worker_id).encode(),
-            message + message
-        ])
-        message_id += 1
-        print(f'Sending {message_id}')
+    worker = Worker(length)
+    worker.run()

3df2b167242f182bcc7126722590fb386d9f9720
User3574 <69751521+User3574@users.noreply.github.com>
2023-02-17 10:47:50 +0100

    ADD: Client computation measurement

--- ex_client.py
+++ ex_client.py
@@ -1,13 +1,17 @@
 from src import client
 import json
 import time
+import click
 
-if __name__ == '__main__':
+
+@click.command()
+@click.option('--msg_count', default=100000, help='Amount of messages to send')
+def main(msg_count):
     client = client.Client()
 
     # Create dummy array
     msg_count = 100000
-    array = [[x, x+1] for x in range(msg_count)]
+    array = [[x, x + 1] for x in range(msg_count)]
 
     # Serialize array
     array = [json.dumps(x).encode() for x in array]
@@ -16,5 +20,9 @@ if __name__ == '__main__':
     results = client.compute(array)
     end = time.time()
 
-    print(f'Computation time: {end-start}')
-    #print(results)
+    print(f'Computation time: {end - start} for messages: {msg_count}')
+    # print(results)
+
+
+if __name__ == '__main__':
+    main()
